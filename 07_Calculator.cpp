cec7180dfa3ae66f8ce18c44e6ca8ccae02131cc
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-27 21:44:12 +0800

    commit

--- 07_Calculator.cpp
+++ 07_Calculator.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    int num1, num2;
+    cout<<"Enter Num1: ";
+    cin>>num1;
+    cout<<"Enter Num2: ";
+    cin>>num2;
+    cout<<num1<<" + "<<num2<<" = "<<num1+num2<<endl;
+    cout<<num1<<" - "<<num2<<" = "<<num1-num2<<endl;
+    cout<<num1<<" * "<<num2<<" = "<<num1*num2<<endl;
+    cout<<num1<<" / "<<num2<<" = "<<num1/num2<<endl;
 
-    
+
     return 0;
 }
