150fb394fdd25d20f8bc925f250a79a70c891fcb
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-28 14:34:42 +0800

    commit

--- 19_pointers.cpp
+++ 19_pointers.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 int main()
 {
-    
+   int age = 29;
+   double gpa = 3.9;
+   string name = "Syamil";
 
+   cout<<"Age :"<<&age<<endl; 
+   cout<<"GPA :"<<&gpa<<endl; 
+   cout<<"Name :"<<&name<<endl;
+
+    //pointer-->memory address 
+
+    int *pAge = &age;
+    double *pGpa = &gpa;
+    string *pName = &name;
+
+    cout<<pAge<<endl;
+    cout<<pGpa<<endl;
+    cout<<pName<<endl;
     
     return 0;
 }
