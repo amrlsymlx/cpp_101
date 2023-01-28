127edcc674b973f45af02a4ee5d5fe7898389c01
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-28 14:00:17 +0800

    commit

--- 18_nestedFor.cpp
+++ 18_nestedFor.cpp
@@ -9,8 +9,12 @@ int main()
                             {5,6}
                         };
     
-    cout<<numberGrid[0][1];
+    for(int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            cout<<numberGrid[i][j];
+        }
+        cout<<endl;
+    }
 
-    
     return 0;
 }
