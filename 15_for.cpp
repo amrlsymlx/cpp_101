edc0da69a9fcf727ce36e06c29bafb91a1bd9f8d
A.Syamil <112838819+amrlsymlx@users.noreply.github.com>
2023-01-28 13:32:30 +0800

    commit

---
+++ 15_for.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for(int i=1; i<=100; i++){
+        cout<<i<<endl;
+    }
+    
+    int num[] = {1,2,3,4,5};
+
+    for(int i=0; i<=4; i++){
+        cout<<num[i]<<endl;
+    }
+
+
+    return 0;
+}
